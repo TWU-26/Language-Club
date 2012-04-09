f4adda4233011690ed02019dad2bf39a086e7e94
David Kormushoff <kormie@gmail.com>
2012-04-09 13:03:33 +0530

    Implement equals method

--- Probablility/src/Probability.java
+++ Probablility/src/Probability.java
@@ -1,17 +1,36 @@
 public class Probability {
     private int denominator;
     private int numerator;
+    private final double decimalValue;
 
     public Probability(int numerator, int denominator) {
+        if (numerator > denominator) throw new IllegalArgumentException("You're stupid");
         this.numerator = numerator;
         this.denominator = denominator;
+        decimalValue = (double) this.numerator / this.denominator;
     }
 
     public boolean isProbable() {
-        return true?((double)numerator / denominator >0.5):false;
+        return decimalValue > 0.5;
     }
 
     public boolean isImprobable() {
-        return !this.isProbable();
+        return decimalValue < 0.5;
     }
+
+    public Probability not() {
+        return new Probability(denominator - numerator, denominator);
+    }
+
+    @Override
+    public boolean equals(Object probability) {
+        if (probability instanceof Probability) {
+            return (((Probability) probability).decimalValue == this.decimalValue);
+        } else {
+            return super.equals(probability);
+        }
+    }
+
+    
+
 }
--- Probablility/test/ProbabilityTest.java
+++ Probablility/test/ProbabilityTest.java
@@ -1,17 +1,45 @@
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
 
 public class ProbabilityTest {
-    @Test
-    public void should_be_probable(){
-        assertEquals(false, new Probability(1, 6).isProbable());
-        assertEquals(true, new Probability(3, 4).isProbable());
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throw_exception() {
+        new Probability(3,2);
     }
 
     @Test
-    public void should_be_improbable(){
-        assertEquals(true ,new Probability(1, 6).isImprobable());
-        assertEquals(false ,new Probability(3, 4).isImprobable());
+    public void should_be_probable() {
+        assertTrue(new Probability(7,10).isProbable());
+    }
+
+    @Test
+    public void should_not_be_probable(){
+        assertFalse(new Probability(1,5).isProbable());
+    }
+
+    @Test
+    public void should_be_improbable() {
+        assertTrue(new Probability(3,10).isImprobable());
+    }
+
+    @Test
+    public void should_not_be_improbable() {
+        assertFalse(new Probability(4,5).isImprobable());
+    }
+
+    @Test
+    public void five_should_be_neither_probable_nor_improbable(){
+        assertFalse(new Probability(1,2).isProbable());
+        assertFalse(new Probability(1,2).isImprobable());
+    }
+    
+    @Test
+    public void should_return_opposite_probability(){
+        assertThat(new Probability(1,4).not(), equalTo(new Probability(3,4)));
+        assertThat(new Probability(3,4).not(), equalTo(new Probability(1,4)));
     }
 }
